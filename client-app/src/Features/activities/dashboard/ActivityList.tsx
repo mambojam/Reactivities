d2b8bf799201e3d9024b141aeb69d0a3b0d102c8
Jacobs <Harry.Jacobs@liberata.com>
2024-10-16 10:15:12 +0100

    Test 2 - linking Github account to vsCode

--- client-app/src/Features/activities/dashboard/ActivityList.tsx
+++ client-app/src/Features/activities/dashboard/ActivityList.tsx
@@ -43,7 +43,6 @@ export default function ActivityList({activities, selectActivity, deleteActivity
                     </Item>
                 ))}
             </Item.Group>
-            dsg
         </Segment>
     )
 }
