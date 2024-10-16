edd477ad250ceb38511d1cb0a76ff93fa6f89e11
Jacobs <Harry.Jacobs@liberata.com>
2024-10-16 10:11:50 +0100

    Test

--- client-app/src/Features/activities/dashboard/ActivityList.tsx
+++ client-app/src/Features/activities/dashboard/ActivityList.tsx
@@ -43,6 +43,7 @@ export default function ActivityList({activities, selectActivity, deleteActivity
                     </Item>
                 ))}
             </Item.Group>
+            dsg
         </Segment>
     )
 }
